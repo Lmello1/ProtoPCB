0663c66a51b442840bac0bb4346bff8533701e45
Landon Mello <LMoperatingsystem@gmail.com>
2022-07-26 23:20:47 -0700

    PCB rev 1.0! (do not use/under heavy development)

--- SRC/LED_matrix_test/LED_matrix_test.ino
+++ SRC/LED_matrix_test/LED_matrix_test.ino
@@ -1,7 +1,9 @@
 const int latchPin = 12;
 const int clockPin = 13;
 const int dataPin = 9;
-
+const int Button1 = 15;
+const int Button2 = 16;
+const int Button3 = 17;
 
 
  
@@ -10,26 +12,76 @@ void setup ()
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
+  pinMode(Button1, INPUT_PULLUP);
+  pinMode(Button2, INPUT_PULLUP);
+  pinMode(Button3, INPUT_PULLUP);
 }
-void loop()
-{
+void loop()  {
     digitalWrite(latchPin, LOW);
  
     shiftOut(dataPin, clockPin, MSBFIRST, B10101010);
 
     shiftOut(dataPin, clockPin, MSBFIRST, B10101010);
-  
-    digitalWrite(latchPin, HIGH);
- 
-    delay(500);
 
-    digitalWrite(latchPin, LOW);
- 
     shiftOut(dataPin, clockPin, MSBFIRST, B01010101);
 
     shiftOut(dataPin, clockPin, MSBFIRST, B01010101);
   
     digitalWrite(latchPin, HIGH);
  
-    delay(500);
+    delay(250);
+
+    digitalWrite(latchPin, LOW);
+ 
+    shiftOut(dataPin, clockPin, MSBFIRST, B01010101);
+
+    shiftOut(dataPin, clockPin, MSBFIRST, B01010101);
+
+    shiftOut(dataPin, clockPin, MSBFIRST, B01010101);
+
+    shiftOut(dataPin, clockPin, MSBFIRST, B01010101);
+  
+    digitalWrite(latchPin, HIGH);
+ 
+    delay(250);
+
+    digitalWrite(latchPin, LOW);
+ 
+    shiftOut(dataPin, clockPin, MSBFIRST, B01010101);
+
+    shiftOut(dataPin, clockPin, MSBFIRST, B01010101);
+
+    shiftOut(dataPin, clockPin, MSBFIRST, B10101010);
+
+    shiftOut(dataPin, clockPin, MSBFIRST, B10101010);
+
+    digitalWrite(latchPin, HIGH);
+ 
+    delay(250);
+
+    digitalWrite(latchPin, LOW);
+ 
+    shiftOut(dataPin, clockPin, MSBFIRST, B10101010);
+
+    shiftOut(dataPin, clockPin, MSBFIRST, B10101010);
+
+    shiftOut(dataPin, clockPin, MSBFIRST, B10101010);
+
+    shiftOut(dataPin, clockPin, MSBFIRST, B10101010);
+
+    digitalWrite(latchPin, HIGH);
+ 
+    delay(250);
+
+    digitalWrite(latchPin, LOW);
+ 
+    shiftOut(dataPin, clockPin, MSBFIRST, B00000000);
+
+    shiftOut(dataPin, clockPin, MSBFIRST, B00000000);
+
+    shiftOut(dataPin, clockPin, MSBFIRST, B00000000);
+
+    shiftOut(dataPin, clockPin, MSBFIRST, B00000000);
+
+    digitalWrite(latchPin, HIGH);
 }
