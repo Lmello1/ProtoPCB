cdd47178143d2ce0b9b98107b7c00a83933d0c95
Bendi11 <bkliebmann@gmail.com>
2022-07-27 14:12:28 -0700

    Update screen buffer to support atomic + more efficient operations
    - `rowmask_t` converted to `uint8_t` and `screen_buf` now contains 32 elements, 2 for each row
     - uint8_t operations are atomic on the 8-bit attiny3227, so can be safely executed without the need for an ATOMIC_BLOCK
     - 8 bit elements can be modified in 3 instructions (load + modify + store), saving instructions compared to 16 bit elements, which had unneeded instructions for loading two 8-bit values
     - GCC attributes added to functions forcing inlining, from compiler explorer output each call to the `set` and `unset` functions should now take 3 instructions
     - Split matrix code into a separate file to aid readability
     - Renamed button global variables and converted them from `const int`s to `constexpr uint8_t`s

---
+++ SRC/LED_matrix_test/matrix.cpp
@@ -0,0 +1,28 @@
+#include "matrix.hpp"
+
+#include <Arduino.h>
+#include <util/atomic.h>
+
+volatile matrix::rowmask_t matrix::screen_buf[32] = {0};
+
+/** 
+ * \brief Interrupt triggered by timer b that is used to display the LED matrix row by row
+ */
+ISR(TCB0_INT_vect) {
+    static uint8_t row = 0;
+    //Increment the currently displayed row or reset to the first row if we have displayed the last one
+    row = (row >= 32) ? 0 : row + 2;
+    const uint16_t rowbits = 0b1000000000000000 >> row;
+    digitalWrite(LATCH_PIN, LOW);
+    //write lower 8 row bits to U4
+    shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, ~(uint8_t)(rowbits));
+    //write upper 8 row bits to U3
+    shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, ~(uint8_t)(rowbits >> 8));
+
+    //write lower 8 column bits to U2
+    shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, matrix::screen_buf[row]);
+    //write upper 8 column bits to U1
+    shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, matrix::screen_buf[row + 1]);
+
+    digitalWrite(LATCH_PIN, HIGH);
+}
---
+++ SRC/LED_matrix_test/matrix.hpp
@@ -0,0 +1,56 @@
+#pragma once
+#include <stdint.h>
+
+static constexpr uint8_t LATCH_PIN = 12;
+static constexpr uint8_t CLOCK_PIN = 13;
+static constexpr uint8_t DATA_PIN = 9;
+
+namespace matrix {
+
+/** \brief Constant passed to the `set` function to turn an LED on, so we can eliminate an unneeded bitshift */
+static constexpr uint8_t LED_ON = 0b10000000;
+
+/** 
+ * How a half of a single row of the LED matrix is stored,
+ * here every bit in the number indicates if the LED should be on or off
+ */
+using rowmask_t = uint8_t;
+
+/** 
+ * \brief Buffer containing bitmasks for each row in the matrix display.
+ * The reason that 32 uint8_ts are stored instead of 16 uint16_ts is because operations on uint8_ts are atomic,
+ * meaning they cannot be interrupted by the screen rendering interrupt. So, we can modify portions of the display buffer without needing an `ATOMIC_BLOCK`
+ */
+extern volatile rowmask_t screen_buf[32];
+
+/**
+ * Set an LED in the matrix display by (x, y) coordinates
+ */
+__attribute__((always_inline))
+inline void set(uint8_t x, uint8_t y) {
+    uint8_t i = y * 2 + (x > 3 ? 1 : 0);
+    screen_buf[i] |= (LED_ON >> x);
+}
+
+/**
+ * Set or unset an LED in the matrix display by (x, y) coordinates
+ */
+__attribute__((always_inline))
+inline void unset(uint8_t x, uint8_t y) {
+    uint8_t i = y * 2 + (x > 3 ? 1 : 0);
+    screen_buf[i] &= ~(LED_ON >> x);
+}
+
+/**
+ * Set the contents of an entire row to the bitmask specified. Note that this function is **not** atomic and
+ * must be surrounded by an `ATOMIC_BLOCK` to ensure no garbled screen data is present
+ * \param row The row index from 0-15 inclusive
+ * \param val Bitmap of LED values, e.x.: 0b0101010101010101
+ */
+__attribute__((always_inline))
+inline void setrow(uint8_t row, uint16_t val) {
+    screen_buf[row] = (uint8_t)val;
+    screen_buf[row + 1] = (uint8_t)(val >> 8);
+}
+
+}
